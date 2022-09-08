ff7653d6b8beb55258adeb8491805a02758a36d4
Eric Chan <ericchan024@gmail.com>
2022-09-08 12:39:34 -0400

    added buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,21 +1,13 @@
-// create an array containing the strings, rock, paper, and scissor
 let choices = ['ROCK', 'PAPER', 'SCISSORS'];
 
-// will return a random variable indicating rock, paper, or scissor
 function getComputerChoice() {
-    // using the random function, select a random value from the list
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    // return the selected value
     return computerChoice;
 }
 
-// function takes two parameters, one is the player's selection and the other is computer's selection
 function playRound(playerSelection, computerSelection) {
-    // make both the playerSelection and computerSelection to uppercase so we can do case
-    // insensitive comparison
     playerSelection = playerSelection.toUpperCase();
 
-    // check if playerSelection beats computerSelection through comparison
     if (playerSelection === computerSelection) {
         return 'Draw!'
     } else if (playerSelection === 'ROCK') {
@@ -44,15 +36,19 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    // create a variable playerSelection to ask user for rock paper or scissors
-    // we will use this variable when we call playRounds
-    // create variable 'rounds' to keep track of number of rounds
     let rounds = 1;
-    // in a while loop, keep calling playRound until rounds is equal to 5
     while (rounds <= 5) {
         let playerSelection = prompt(`Round ${rounds}: ROCK, PAPER, or SCISSORS?`, 'ROCK');
         console.log(playRound(playerSelection, getComputerChoice()));
         rounds++;
     }
     return 'Good game!'
+    
 }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
